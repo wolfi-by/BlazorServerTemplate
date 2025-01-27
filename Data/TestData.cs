9b15a9eedee234acffcacc1c118b20db4523ef6a
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2025-01-27 06:57:06 +0100

    update

---
+++ Data/TestData.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+
+namespace BlazorServerTemplate.Data
+{
+    public class TestData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
