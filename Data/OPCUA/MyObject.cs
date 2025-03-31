2d7d12cb885735ec811147e4102bb16b01fa0966
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2025-03-31 19:31:53 +0200

    updates

---
+++ Data/OPCUA/MyObject.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+
+namespace BlazorServerTemplate.Data.OPCUA
+{
+    public class MyObject
+    {
+        public double MyVariable1 { get; set; }
+        public Int64 MyVariable2 { get; set; }
+        public Int64 MyVariable3 { get; set; }
+        public bool MyVariable4 { get; set; }
+    }
+}
