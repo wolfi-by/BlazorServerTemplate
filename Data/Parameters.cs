5392a5bd29b6c1e6dfa96908ecb588b9e496a28c
Wolfgang Höhne <wolfi-by@users.noreply.github.com>
2025-05-02 00:29:33 +0200

    wip

---
+++ Data/Parameters.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+
+namespace BlazorServerTemplate.Data
+{
+    public class Parameters
+    {
+        public Int64 Temperature { get; set; }
+    }
+}
